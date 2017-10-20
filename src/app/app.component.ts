bf678cf8225a5a440bf94a5f0bf310d6880587ec
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2017-10-20 02:16:57 -0400

    Up to part 3

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,32 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'app';
+  title = 'Tour of Heroes';
+  hero: Hero = {
+  	id: 1,
+  	name: 'Mr. Potato Head'
+  };
 }
+
+export class Hero {
+	id: number;
+	name: string;
+}
+
+
+const HEROES: Hero[] = [
+	{id: 11, name: 'Claudia'},
+	{id: 12, name: 'Fiona'},
+	{id: 13, name: 'Bryce'},
+	{id: 14, name: 'Eli'},
+	{id: 15, name: 'Zaque'},
+	{id: 16, name: 'Ryan'},
+	{id: 17, name: 'Theresa'},
+	{id: 18, name: 'Matt'},
+	{id: 19, name: 'Justin'},
+	{id: 20, name: 'Alican'}
+];
+
+heroes = HEROES;
