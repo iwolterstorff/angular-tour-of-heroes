2a83b5ac5a240ba8d441e56a5b7182fd270b92d9
Ian Wolterstorff <iwolterstorff@ccs.neu.edu>
2017-10-23 15:13:47 -0400

    Added a bunch of styling

--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -13,25 +13,25 @@ import { HeroService } from './hero.service';
 
 export class HeroesComponent implements OnInit {
   heroes: Hero[];
-
   selectedHero: Hero;
 
+  constructor(
+    private router: Router,
+    private heroService: HeroService) { }
+
+  getHeroes(): void {
+    this.heroService.getHeroes().then(heroes => this.heroes = heroes);
+  }
+
   ngOnInit(): void {
-  	this.getHeroes();
+    this.getHeroes();
   }
 
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  constructor(private heroService: HeroService, private router: Router) {}
-
-  goToDetail(): void {
-  	this.router.navigate(['/detail', this.selectedHero.id]);
-  }
-
-  getHeroes(): void {
-  	this.heroService.getHeroes().then(heroes => this.heroes = heroes);
-  	//this.heroService.getHeroesSlowly().then(heroes => this.heroes = heroes);
+  gotoDetail(): void {
+    this.router.navigate(['/detail', this.selectedHero.id]);
   }
 }
